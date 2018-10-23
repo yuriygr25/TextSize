7cff449569f67b821e6b0ba5a9357a682d4d5a0e
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-24 01:23:52 +0300

    commit

---
+++ app/src/main/java/com/example/yura/textsize/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.yura.textsize;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getActionBar().hide();
+        //getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        //getActionBar().setIcon(getResources().getDrawable(R.drawable.ic_launcher));
+        //getActionBar().setIcon(R.drawable.ic_launcher);
+
+/*
+        LinearLayout ivTarget = (LinearLayout) findViewById(R.id.LinearLayout1);
+
+        ivTarget.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher)); // присвоим второму
+*/
+
+//        getActionBar().setIcon(R.drawable.ic_android_black_24dp);
+    }
+}
